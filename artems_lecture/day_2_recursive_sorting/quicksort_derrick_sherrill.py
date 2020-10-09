d2b1acca23984d4dbc2b1d9dc9683adda288ce6a
RMDircio <reginadircio@gmail.com>
2020-10-08 20:49:59 -0500

    YouTube help

---
+++ artems_lecture/day_2_recursive_sorting/quicksort_derrick_sherrill.py
@@ -0,0 +1,8 @@
+'''   Derrick Sherrill Youtube https://www.youtube.com/watch?v=kFeXwkgnQ9U '''
+
+###   QUICK SORT   ###
+## Iterative method ##
+
+
+# takes an unsorted list
+def quick_sort(sequence):
